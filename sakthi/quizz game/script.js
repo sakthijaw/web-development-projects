592ebee9ff633d70b87f90d5a540e7bcd5b9ee4e
Sakthi J <167529826+sakthijaw@users.noreply.github.com>
2025-06-21 20:59:07 +0530

    Add files via upload

---
+++ sakthi/quizz game/script.js
@@ -0,0 +1,6 @@
+const startScreen=document.getElementById("start-screen")
+const quizScreen=document.getElementById("quiz-screen")
+const resultScreen=document.getElementById("result-screen")
+const questionText=document.getElementById("question-text")
+const 
+
